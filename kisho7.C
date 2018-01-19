5e4371e07376737811d28fc325fa05b299e361b0
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-01-19 16:47:13 +0530

    Update kisho7.C

--- kisho7.C
+++ kisho7.C
@@ -1,10 +1,10 @@
 #include <stdio.h>
 void printName(char* name,int count) 
 { 
-    printf("%03d : %s\n",count+1,name); 
-    count+=1; 
-    if(count<10) 
-        printName(name,count); 
+ printf("%03d : %s\n",count+1,name); 
+ count+=1; 
+ if(count<10) 
+ printName(name,count); 
 } 
 int main() 
 { 
