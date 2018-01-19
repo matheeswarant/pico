3a2dea28232b18da3cca1e004968b62761809ee0
kishorepriyan <35127853+kishorepriyan@users.noreply.github.com>
2018-01-19 16:44:07 +0530

    Update kisho5.C

--- kisho5.C
+++ kisho5.C
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-int main(){
-    
+int main()
+{
     int a, b, c;
     printf("Enter a,b,c: ");
     scanf("%d %d %d", &a, &b, &c);
-    
     if (a > b && a > c) {
         printf("a is Greater than b and c");
     }
